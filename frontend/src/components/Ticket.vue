44895ed7f8957b216dd0c0d9f8f716faaafb4bed
RosaTZ <rosalbinatorreszarate@gmail.com>
2023-08-14 23:18:17 -0500

    commit 5 Rosa

--- frontend/src/components/Ticket.vue
+++ frontend/src/components/Ticket.vue
@@ -31,6 +31,7 @@
                 <th>Vehiculo</th>
                 <th>Puesto</th>
                 <th>Precio</th>
+                <th>Boleto</th>
               </tr>
             </thead>
             <tbody>
@@ -43,6 +44,7 @@
                 <td>{{ t.vehiculo.num_vehiculo }} {{ t.vehiculo.placa }}</td>
                 <td>{{ t.numero_puesto }}</td>
                 <td>{{ t.precio }}</td>
+                <td @click="Buscarboleto(t)">🖨️</td>
               </tr>
             </tbody>
           </table>
@@ -54,8 +56,8 @@
     <!--  -->
     <div class="modal-bg" id="modal" v-if="modalBuscar === true">
       <div class="modal-content">
-        <div class="modal-body" v-for="bt in buscarTick" :key="bt">
-          <p id="tituloTabla">Ticket # {{ bt.codigo }}</p>
+        <div class="modal-body" v-for="t in buscarTick" :key="t">
+          <p id="tituloTabla">Ticket # {{ t.codigo }}</p>
           <div class="table-container">
             <table class="custom-table">
               <thead>
@@ -68,18 +70,20 @@
                   <th>Vehiculo</th>
                   <th>Puesto</th>
                   <th>Precio</th>
+                  <th>Boleto</th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="(bt, i) in buscarTick" :key="i">
-                  <td>{{ bt.codigo }}</td>
-                  <td>{{ bt.cliente.cedula }}</td>
-                  <td>{{ bt.cliente.nombre }} {{ bt.cliente.apellidos }}</td>
-                  <td>{{ bt.ruta.origen }}</td>
-                  <td>{{ bt.ruta.destino }}</td>
-                  <td>{{ bt.vehiculo.num_vehiculo }} {{ bt.vehiculo.placa }}</td>
-                  <td>{{ bt.numero_puesto }}</td>
-                  <td>{{ bt.precio }}</td>
+                <tr v-for="(t, i) in buscarTick" :key="i">
+                  <td>{{ t.codigo }}</td>
+                  <td>{{ t.cliente.cedula }}</td>
+                  <td>{{ t.cliente.nomre }} {{ t.cliente.apellidos }}</td>
+                  <td>{{ t.ruta.origen }}</td>
+                  <td>{{ t.ruta.destino }}</td>
+                  <td>{{ t.vehiculo.num_vehiculo }} {{ t.vehiculo.placa }}</td>
+                  <td>{{ t.numero_puesto }}</td>
+                  <td>{{ t.precio }}</td>
+                  <td @click="Buscarboleto(t)">🖨️</td>
                 </tr>
               </tbody>
             </table>
@@ -92,6 +96,67 @@
         </div>
       </div>
     </div>
+    <!--  -->
+     <!-- Boleto -->
+<div v-if="modalBoleto===true">
+    <div class="ticket" v-for="v in boleto" :key="v">
+    <div class="ticket-header">
+      <h4>{{ v.empresa.nombre }}</h4>
+      <p>{{ v.empresa.nit }}</p>
+      <p>{{ v.empresa.direccion }}</p>
+      <p>{{ v.empresa.telefono }}</p>
+    </div>
+    <div class="ticket-info">
+      <div>
+        <label>Fecha de Venta:</label>
+        <span> {{ v.fecha_venta }}</span>
+      </div>
+      <div>
+        <label>Fecha de Salida:</label>
+        <span>{{ v.fecha_salida }}/{{ v.hora_salida }}</span>
+      </div>
+      <div>
+        <label>Cédula del Pasajero:</label>
+        <span>{{ v.cliente.cedula }}</span>
+      </div>
+      <div>
+        <label>Nombre del Pasajero:</label>
+        <span>{{ v.cliente.nombre }} {{ v.cliente.apellidos }}</span>
+      </div>
+      <div>
+        <label>Teléfono:</label>
+        <span>{{ v.cliente.telefono }}</span>
+      </div>
+      <div>
+        <label>Vehículo:</label>
+        <span>{{ v.vehiculo.placa }}/{{ v.vehiculo.num_vehiculo }}</span>
+      </div>
+      <div>
+        <label>Origen:</label>
+        <span>{{ v.ruta.origen }}</span>
+      </div>
+      <div>
+        <label>Destino:</label>
+        <span>{{ v.ruta.destino }}</span>
+      </div>
+      <div>
+        <label>Número de Ticket:</label>
+        <span>{{ v.codigo }}</span>
+      </div>
+      <div>
+        <label>Silla:</label>
+        <span>{{ v.numero_puesto }}</span>
+      </div>
+      <div>
+        <label>Valor del Ticket:</label>
+        <span>{{ v.precio }}</span>
+      </div>
+    </div>
+    <div class="ticket-footer">
+      <p>¡Gracias por su compra!</p>
+    </div>
+  </div>
+</div>
     <!--  -->
   </div>
 </template>
@@ -108,6 +173,9 @@ let buscarTick = ref([]);
 let modalBuscar = ref(false);
 let alert=ref(false)
 let alertE=ref(false)
+let codigoBoleto=ref('')
+let boleto=ref([])
+let modalBoleto=ref(false)
 
 async function buscarTicket() {
   tickets.value = await useTicket.buscarTicket();
@@ -131,6 +199,13 @@ await useTicket
         console.log(error);
       }
     });
+}
+async function Buscarboleto(t){
+codigoBoleto.value=t.codigo
+await useTicket.buscarTicketId(codigoBoleto.value).then((res)=>{
+boleto.value=res.ticket
+modalBoleto.value=true
+  })
 }
 function alerta() {
   setTimeout(() => {
