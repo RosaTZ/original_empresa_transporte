ea3eaf52da7a2871d860b5d00b6746e9c0e58340
RosaTZ <rosalbinatorreszarate@gmail.com>
2023-08-18 16:14:12 -0500

    ddddd

--- backend/models/server.js
+++ backend/models/server.js
@@ -36,6 +36,7 @@ conectarDB(){
     middlewares(){
         this.app.use(express.json())
         this.app.use(cors())
+        this.app.use(express.static('public'))
     }
     escuchar(){
         this.app.listen(process.env.PORT, () => {
